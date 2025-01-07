b7390458c7e1638098d58f3695e739c6da4d5ea7
윤수현 <ysh@knu.ac.kr>
2025-01-07 15:56:39 +0900

    feat: Add player animation
    Image change made when going left and right
    
    Co-Authored-By: jkh0515 <81910107+jkh0515@users.noreply.github.com>

--- Assets/Script/player_controller.cs
+++ Assets/Script/player_controller.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
+    private Animator am;
 
-    private void Start()
-    {
+    private void Start() {
         rb = GetComponent<Rigidbody2D>();
+        am = GetComponent<Animator>();
     }
 
-    private void FixedUpdate()
-    {
-        float moveInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+    private void FixedUpdate() {
+        if (Input.GetKey(KeyCode.A)) {
+            am.SetInteger("input", 2);
+            rb.AddForce(transform.right * -1 * moveSpeed, ForceMode2D.Impulse);
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            am.SetInteger("input", 1);
+            rb.AddForce(transform.right * moveSpeed, ForceMode2D.Impulse);
+        }
+        else 
+            am.SetInteger("input", 0);
     }
 }
