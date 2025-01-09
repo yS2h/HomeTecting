36e77e042a8e3079df69259bdc2bfd6f66d3437c
윤수현 <ysh@knu.ac.kr>
2025-01-09 16:07:39 +0900

    feat: Add clock system
    Co-Authored-By: jkh0515 <81910107+jkh0515@users.noreply.github.com>

---
+++ Assets/Script/time_manager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class time_manager : MonoBehaviour {
+    public static time_manager instance;
+    public float sec;
+
+    void Awake() {
+        if (instance != null) {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start() {
+        sec = 0;
+    }
+
+    void Update() {
+        sec += Time.deltaTime;
+        Debug.Log(sec);
+        transform.rotation = Quaternion.Euler(0, 0, sec - 90);
+    }
+}
