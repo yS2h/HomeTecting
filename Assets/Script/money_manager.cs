bb887de7ffb4ce6c74d7ad30fa02089243100167
윤수현 <ysh@knu.ac.kr>
2025-01-14 16:59:07 +0900

    feat: Add money system
    Co-Authored-By: jkh0515 <81910107+jkh0515@users.noreply.github.com>

---
+++ Assets/Script/money_manager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class money_manager : MonoBehaviour
+{
+    public static money_manager instance;
+    public TMP_Text moneyText, moneyText2;
+    public int money = 1000;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        moneyText.text = "" + money;
+        moneyText2.text = "" + money;
+    }
+
+    void Update()
+    {
+        if (!time_manager.instance.pause)
+        {
+            money++;
+        }
+        moneyText.text = "" + money;
+        moneyText2.text = "" + money;
+    }
+}
