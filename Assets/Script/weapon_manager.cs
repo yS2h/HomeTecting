7a0cc85e3f99c1be7e3eb80336da66b0324b981b
윤수현 <ysh@knu.ac.kr>
2025-01-22 15:53:59 +0900

    feat: Add house interaction
    Co-Authored-By: jkh0515 <81910107+jkh0515@users.noreply.github.com>

--- Assets/Script/weapon_manager.cs
+++ Assets/Script/weapon_manager.cs
@@ -8,31 +8,44 @@ public class weapon_manager : MonoBehaviour
 {
     bool[] weapons = new bool[11];
     int[] prices = new int[11] {3000, 5000, 7000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000};
-    private bool setWeapon = false;
-    private GameObject storageDoor, keyF;
-    private GameObject defaultCanvas, storageCanvas;
+    private bool getWeapon = false, setWeapon = false;
+    private GameObject storageDoor, houseDoor, keyF;
+    private GameObject defaultCanvas, storageCanvas, houseCanvas;
     public TMP_Text closeButtonText;
     public TMP_Text[] weaponButtonText = new TMP_Text[11];
 
     void Start()
     {
         storageDoor = GameObject.Find("storage_door");
+        houseDoor = GameObject.Find("house_door");
         defaultCanvas = GameObject.Find("default_canvas");
         storageCanvas = GameObject.Find("storage_canvas");
+        houseCanvas = GameObject.Find("house_canvas");
         keyF = transform.GetChild(0).gameObject;
         defaultCanvas.SetActive(true);
         storageCanvas.SetActive(false);
+        houseCanvas.SetActive(false);
         keyF.SetActive(false);
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F) && setWeapon)
-        {
-            //Debug.Log("open weapon");
-            storageCanvas.SetActive(true);
-            defaultCanvas.SetActive(false);
-            time_manager.instance.pause = true;
+        if (Input.GetKeyDown(KeyCode.F))
+        {   
+            if (getWeapon)
+            {
+                //Debug.Log("open weapon");
+                storageCanvas.SetActive(true);
+                defaultCanvas.SetActive(false);
+                time_manager.instance.pause = true;
+            }
+            else if (setWeapon)
+            {
+                //Debug.Log("open house");
+                houseCanvas.SetActive(true);
+                defaultCanvas.SetActive(false);
+                time_manager.instance.pause = true;
+            }
         }
     }
 
@@ -41,14 +54,26 @@ public class weapon_manager : MonoBehaviour
         if (col.tag == "storage")
         {
             keyF.gameObject.SetActive(true);
-            setWeapon = true;
+            getWeapon = true;
             //Debug.Log(money_manager.instance.money);
+        }
+
+        else if (col.tag == "house")
+        {
+            keyF.gameObject.SetActive(true);
+            setWeapon = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D col)
     {
         if (col.tag == "storage")
+        {
+            keyF.gameObject.SetActive(false);
+            getWeapon = false;
+        }
+
+        else if (col.tag == "house")
         {
             keyF.gameObject.SetActive(false);
             setWeapon = false;
@@ -58,6 +83,7 @@ public class weapon_manager : MonoBehaviour
     public void pushCloseButton()
     {
         storageCanvas.SetActive(false);
+        houseCanvas.SetActive(false);
         defaultCanvas.SetActive(true);
         time_manager.instance.pause = false;
     }
