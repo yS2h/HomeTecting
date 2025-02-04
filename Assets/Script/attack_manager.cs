cd5171a1f332919f128513f476319da4e6377afa
윤수현 <ysh@knu.ac.kr>
2025-02-04 17:32:37 +0900

    feat: Add monster targeting system
    Co-Authored-By: jkh0515 <81910107+jkh0515@users.noreply.github.com>

---
+++ Assets/Script/attack_manager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class attack_manager : MonoBehaviour
+{
+    protected static GameObject targetMonster;
+
+    private GameObject liveMonster;
+
+    private GameObject findTarget()
+    {
+        if (liveMonster == null) return null;
+
+        Transform[] children = liveMonster.GetComponentsInChildren<Transform>();
+        if (children.Length <= 1) return null;
+
+        Transform leftmostChild = null;
+        float minX = float.MaxValue;
+
+        for (int i = 1; i < children.Length; i++) 
+        {
+            float currentX = children[i].position.x;
+            if (currentX < minX)
+            {
+                minX = currentX;
+                leftmostChild = children[i];
+            }
+        }
+
+        return leftmostChild?.gameObject;
+    }
+
+    void Start()
+    {
+        liveMonster = GameObject.Find("live_monster");
+    }
+
+    void Update()
+    {
+        targetMonster = findTarget();
+        //Debug.Log(targetMonster.name);
+    }
+}
