6fb21d488bf4f99b46d281b48cde5e2cd34250b9
윤수현 <ysh@knu.ac.kr>
2025-02-10 17:59:13 +0900

    feat: Functionalize object activation when selecting weapons
    Co-Authored-By: jkh0515 <81910107+jkh0515@users.noreply.github.com>

--- Assets/Script/weapon_manager.cs
+++ Assets/Script/weapon_manager.cs
@@ -8,15 +8,16 @@ public class weapon_manager : MonoBehaviour
 {
     const int weaponNum = 12; // 무기 총 개수
 
-    bool[] weaponNow = new bool[weaponNum]; // 현재 무기를 구매한 상태인지
+    bool[] weaponNow = new bool[weaponNum] { true, true, false, false, false, true, false, true, false, false, true, true }; // 현재 무기를 구매한 상태인지
     int[] weaponPosition = new int[weaponNum] { 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3 }; // 무기를 장착 할 수 있는 곳 번호
     int[] prices = new int[weaponNum] { 3000, 5000, 7000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 5000}; // 무기 가격
     public static bool getWeapon = false, setWeapon = false, setPoint1 = false, setPoint2 = false, setPoint3 = false;
     private int[] selectedWeapon = new int[3] { -1, -1, -1 };
-    private GameObject storageDoor, houseDoor, pointButton, weaponImage;
+    private GameObject storageDoor, houseDoor, pointButton, weaponImage, weaponList;
     private GameObject defaultCanvas, storageCanvas, houseCanvas, pointCanvas;
     private TMP_Text weaponInfoText;
     public TMP_Text[] weaponButtonText = new TMP_Text[weaponNum];
+    private GameObject[] weaponObjectList = new GameObject[weaponNum];
 
     private weapon[] weaponClass = new weapon[weaponNum];
 
@@ -53,10 +54,19 @@ public class weapon_manager : MonoBehaviour
         pointButton = GameObject.Find("point_weapon_button");
         weaponImage = GameObject.Find("weapon_image");
         weaponInfoText = GameObject.Find("weapon_info_text").GetComponent<TMP_Text>();
+        weaponList = GameObject.Find("weapon_list");
+
         defaultCanvas.SetActive(true);
         storageCanvas.SetActive(false);
         houseCanvas.SetActive(false);
         pointCanvas.SetActive(false);
+        weaponList.SetActive(true);
+
+        for (int i = 0; i < weaponNum; i++)
+        {
+            weaponObjectList[i] = weaponList.transform.GetChild(i).gameObject;
+            weaponObjectList[i].SetActive(false);
+        }
 
         weaponClass[0] = new weapon("garlic", 10, 10, 10, 10, "i'm garlic");
         weaponClass[1] = new weapon("ax", 10, 10, 10, 10, "i'm ax");
@@ -154,6 +164,38 @@ public class weapon_manager : MonoBehaviour
         pointCanvas.SetActive(false);
         defaultCanvas.SetActive(true);
         time_manager.isPaused = false;
+    }
+
+    public void pushConfirmButton()
+    {
+        pushCloseButton();
+        if (setPoint1)
+        {
+            if (selectedWeapon[0] == -1) 
+                return;
+            activeWeapon(1, selectedWeapon[0]);
+        }
+        if (setPoint2)
+        {
+            if (selectedWeapon[1] == -1) 
+                return;
+            activeWeapon(2, selectedWeapon[1]);
+        }
+        if (setPoint3)
+        {
+            if (selectedWeapon[2] == -1) 
+                return;
+            activeWeapon(3, selectedWeapon[2]);
+        }
+    }
+
+    public void activeWeapon(int n, int index)
+    {
+        for (int i = 0; i < weaponNum; i++)
+        {
+            if (n == weaponPosition[i])
+                weaponObjectList[i].SetActive(i == index);
+        }
     }
 
     public void pushWeaponButton(int n)
